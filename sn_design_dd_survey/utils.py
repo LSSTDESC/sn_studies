e3421aad221ce7dd52c0f8eefddb4a6c04c8e67b
pgris <gris@clermont.in2p3.fr>
2019-12-20 17:27:48 +0100

    Add missing class Nvisits_m5

--- sn_design_dd_survey/utils.py
+++ sn_design_dd_survey/utils.py
@@ -32,3 +32,33 @@ def flux5_to_m5(bands):
 
 
     return f5_dict
+
+
+def m5_to_flux5(bands):
+    """
+    Function to estimate 5-sigma fluxes from m5 values
+
+    Parameters
+    ----------
+    bands: str
+     filters considered
+
+    Returns
+    -------
+    f5_dict: dict
+     keys = bands
+     values = interp1d(m5,flux_5)
+    """
+    m5_range = np.arange(20.,28.0,0.01)
+    
+    #estimate the fluxes corresponding to this range
+    
+    telescope = Telescope(airmass=1.2)
+    
+    m5_dict={}
+    for band in bands:
+        flux_5 = telescope.mag_to_flux_e_sec(m5_range,[band]*len(m5_range),[30.]*len(m5_range))[:,1]
+        m5_dict[band] = interpolate.interp1d(m5_range,flux_5,bounds_error=False,fill_value=0.0)
+
+
+    return m5_dict
--- sn_design_dd_survey/wrapper.py
+++ sn_design_dd_survey/wrapper.py
@@ -4,6 +4,7 @@ import numpy as np
 
 from .signal_bands import RestFrameBands,SignalBand
 from .ana_file import AnaMedValues,Anadf
+from .utils import flux5_to_m5,m5_to_flux5
 from sn_tools.sn_io import loopStack
 
 
@@ -333,3 +334,321 @@ class Nvisits_cadence:
             axb.set_xlim([0.3,0.85])
             axb.set_xlabel(r'z')
             axb.set_ylabel(r'$\Delta$Number of visits')
+
+class Nvisits_m5:
+    def __init__(self, tab, med_m5):
+
+        cols = tab.columns[tab.columns.str.startswith('m5calc')].values
+
+        self.bands = ''.join([col.split('_')[-1] for col in cols])
+        self.f5_to_m5 = flux5_to_m5(self.bands)
+        self.m5_to_f5 = m5_to_flux5(self.bands)
+        self.snr = tab
+
+        self.med_m5 = self.transform(med_m5)
+
+        print('here medians',self.med_m5)
+
+        idx = tab['z']>=0.2
+        tab = tab[idx]
+
+        self.nvisits = self.estimateVisits()
+
+
+    def estimateVisits(self,):
+
+        dict_cad_m5 = {}
+        
+        cads = pd.DataFrame(np.arange(0.,10.,1.),columns=['cadence'])
+
+        idx = 0
+
+        #add and index to both df for the merging
+
+        m5 = self.med_m5.copy()
+        snr = self.snr.copy()
+
+        m5.loc[:,'idx'] = idx
+        snr.loc[:,'idx'] = idx
+
+        snr = snr.merge(m5,left_on=['idx'],right_on=['idx'])
+        
+        zvals = snr['z'].unique()
+        
+        df_combi = self.make_combi(zvals,cads) 
+
+        df_merge = snr.merge(df_combi,left_on=['z'],right_on=['z'])
+
+        print(df_merge)
+        cols = []
+        for b in self.bands:
+            df_merge['flux_5_e_sec_{}'.format(b)]=df_merge['flux_5_e_sec_{}'.format(b)]/np.sqrt(df_merge['cadence'])
+            df_merge['m5calc_{}'.format(b)] = self.f5_to_m5[b](df_merge['flux_5_e_sec_{}'.format(b)])
+            df_merge.loc[:,'Nvisits_{}'.format(b)]=10**(0.8*(df_merge['m5calc_{}'.format(b)]-df_merge['m5single_{}'.format(b)]))
+            cols.append('Nvisits_{}'.format(b))
+            df_merge['Nvisits_{}'.format(b)]=df_merge['Nvisits_{}'.format(b)].fillna(0.0)
+           
+        
+        #estimate the total number of visits
+        df_merge.loc[:,'Nvisits'] = df_merge[cols].sum(axis=1)
+        
+
+        return df_merge
+
+
+        """
+        for val in med_m5:
+            for season in sel['season'].values:
+                seas= s[sel['season']==season]
+                tab.loc[:,'season'] = season
+                test=tab.merge(seas,left_on=['season'],right_on=['season'])
+            tab.loc[:,'season'] = val['season']
+        """
+
+        for fieldname in med_m5['fieldname'].unique():
+            idx = med_m5['fieldname']==fieldname
+            sel = med_m5[idx]
+            for season in sel['season'].values:
+                seas= sel[sel['season']==season]
+                tab.loc[:,'season'] = season
+                test=tab.merge(seas,left_on=['season'],right_on=['season'])
+                
+                zvals = test['z'].unique()
+
+                df_combi = self.make_combi(zvals,cads)
+
+                #merge with test
+
+                df_merge = test.merge(df_combi,left_on=['z'],right_on=['z'])
+
+                print(df_merge)
+                cols = []
+                for b in self.bands:
+                    df_merge['flux_5_e_sec_{}'.format(b)]=df_merge['flux_5_e_sec_{}'.format(b)]/np.sqrt(df_merge['cadence'])
+                    df_merge['m5calc_{}'.format(b)] = self.f5_to_m5[b](df_merge['flux_5_e_sec_{}'.format(b)])
+                    df_merge.loc[:,'Nvisits_{}'.format(b)]=10**(0.8*(df_merge['m5calc_{}'.format(b)]-df_merge['m5single_{}'.format(b)]))
+                    
+                    
+                
+                """
+                fig, ax = plt.subplots()
+                figb, axb = plt.subplots()
+                for b in 'grizy':
+                    test['flux_5_e_sec_{}'.format(b)]=test['flux_5_e_sec_{}'.format(b)]/np.sqrt(cadence)
+                    test['m5calc_{}'.format(b)] = self.f5_to_m5[b](test['flux_5_e_sec_{}'.format(b)])
+                    test.loc[:,'Nvisits_{}'.format(b)]=10**(0.8*(test['m5calc_{}'.format(b)]-test['m5single_{}'.format(b)]))
+                    ax.plot(test['z'],test['Nvisits_{}'.format(b)],color=filtercolors[b])
+                    axb.plot(test['z'],test['m5calc_{}'.format(b)],color=filtercolors[b])
+                ax.grid()
+                axb.grid()
+                """
+                #print(test)
+
+                #plt.show()
+    
+
+        self.map_cad_m5 = df_merge
+    
+    def plot_map(self, dft):
+
+        
+        mag_range = [23., 27.5] 
+        dt_range=[0.5, 20.]
+
+        dt = np.linspace(dt_range[0], dt_range[1], 100)
+        m5 = np.linspace(mag_range[0], mag_range[1], 100)
+        
+        
+        zrange = np.arange(0.3,0.9,0.1)
+
+        df = pd.DataFrame()
+        for z in zrange:
+            idb = np.abs(dft['z']-z)<1.e-5
+            df = pd.concat([df,dft[idb]], sort=False)
+
+
+        for b in self.bands: 
+
+            f5 = self.m5_to_f5[b](m5)
+            M5, DT = np.meshgrid(m5, dt)            
+            F5, DT = np.meshgrid(f5, dt)
+            metric = np.sqrt(DT) * F5
+
+            fig = plt.figure(figsize=(8, 6))
+            fig.suptitle('{} band'.format(b))
+            plt.imshow(metric, extent=(mag_range[0],mag_range[1],dt_range[0],dt_range[1]), 
+                       aspect='auto', alpha=0.25)
+
+            idx = np.abs(df['cadence']-1.)<1.e-5
+            dfsel = df[idx]
+
+            dfsel = dfsel.sort_values(by=['flux_5_e_sec_{}'.format(b)])
+            
+            ll = dfsel['flux_5_e_sec_{}'.format(b)].values
+           
+            cs = plt.contour(M5, DT, metric, ll,colors='k')
+            
+            fmt = {}
+            strs = ['$z=%3.2f$' % zz for zz in dfsel['z'].values]
+            for l, s in zip(cs.levels, strs):
+                fmt[l] = s
+            plt.clabel(cs, inline=True, fmt=fmt,
+                       fontsize=16, use_clabeltext=True)
+
+           
+            
+            #for z in df['z'].unique():
+            #    sel = df[np.abs(df['z']-z)<1.e-5]
+            #    plt.plot(sel['m5calc_{}'.format(b)],sel['cadence'],'ro',mfc='None')
+            
+            plt.xlabel('$m_{5\sigma}$', fontsize=18)
+            plt.ylabel(r'Observer frame cadence $^{-1}$ [days]', fontsize=18)
+            plt.xlim(mag_range)
+            plt.ylim(dt_range)
+            plt.grid(1)
+            
+        #plt.show()
+    
+
+    def plot(self, data, cadences, what='m5_calc',legy='m5'):
+
+        fig, ax = plt.subplots()
+        axb = None
+        ls = ['-','--','-.']
+
+        if what == 'Nvisits':
+            figb, axb = plt.subplots()
+            #cols = []
+            #for b in self.bands:
+            #    cols.append('Nvisits_{}'.format(b))
+
+        for io,cad in enumerate(cadences):
+            idx = np.abs(data['cadence']-cad)<1.e-5
+            sel = data[idx]
+
+            for b in self.bands:
+                if io == 0:
+                    ax.plot(sel['z'].values,sel['{}_{}'.format(what,b)].values,color=filtercolors[b],label=b,ls=ls[io])
+                else:
+                   ax.plot(sel['z'].values,sel['{}_{}'.format(what,b)].values,color=filtercolors[b],ls=ls[io]) 
+
+            if what == 'Nvisits':
+                #estimate the total number of visits
+                #sel.loc[:,'Nvisits'] = sel[cols].sum(axis=1)
+                axb.plot(sel['z'].values,sel['Nvisits'].values,
+                         color='k',ls=ls[io],
+                         label='cadence: {} days'.format(int(cad)))
+
+        ax.legend()
+        ax.grid()
+        ax.set_xlabel(r'z')
+        ax.set_ylabel(r'{}'.format(legy))
+        
+        if axb:
+            axb.legend()
+            axb.grid()
+            axb.set_xlabel(r'z')
+            axb.set_ylabel(r'{}'.format(legy))
+
+
+
+    def make_combi(self,zv, cad):
+
+        df = pd.DataFrame()
+
+        for z in zv:
+            dfi = cad.copy()
+            dfi.loc[:,'z'] = z
+            df = pd.concat([df,dfi],ignore_index=True)
+
+        return df
+
+    def transform(self, med_m5):
+ 
+        dictout = {}
+
+        for band in 'grizy':
+            idx = med_m5['filter'] == band
+            sel = med_m5[idx]
+            if len(sel)>0:
+                dictout[band] = [np.median(sel['fiveSigmaDepth'].values)]
+            else:
+                dictout[band] = [0.0]
+
+        
+        
+        return pd.DataFrame({'m5single_g': dictout['g'],
+                             'm5single_r': dictout['r'],
+                             'm5single_i': dictout['i'],
+                             'm5single_z': dictout['z'],
+                             'm5single_y': dictout['y']})
+        """
+        return pd.DataFrame({'m5single_g': [24.331887],
+                             'm5single_r': [23.829639],
+                             'm5single_i': [23.378532],
+                             'm5single_z': [22.861743],
+                             'm5single_y': [22.094841]})
+        """
+    """
+    def transform_old(self, med_m5):
+
+        df = med_m5.copy()
+        
+        #gr = df.groupby(['fieldname','season']).apply(lambda x: self.horizont(x))
+
+        gr = df.groupby(['fieldname']).apply(lambda x: self.horizont(x)).reset_index()
+
+        
+        if 'season' not in gr.columns:
+            gr.loc[:,'season'] = 0
+
+        
+        return pd.DataFrame({'fieldname': ['all'],
+                             'season': [0],
+                             'm5single_g': [0.],
+                             'm5single_r': [23.9632],
+                             'm5single_i': [23.5505],
+                             'm5single_z': [23.0003],
+                             'm5single_y': [22.2338]})
+        
+        return pd.DataFrame({'fieldname': ['all'],
+                             'season': [0],
+                             'm5single_g': [0.],
+                             'm5single_r': [23.829639],
+                             'm5single_i': [23.378532],
+                             'm5single_z': [22.861743],
+                             'm5single_y': [22.094841]})
+        """
+    def horizont(self,grp):
+        
+        dictout = {}
+
+        for band in 'grizy':
+            idx = grp['filter'] == band
+            sel = grp[idx]
+            if len(sel)>0:
+                dictout[band] = [np.median(sel['fiveSigmaDepth'].values)]
+            else:
+                dictout[band] = [0.0]
+
+        
+        return pd.DataFrame({'m5single_g': dictout['g'],
+                             'm5single_r': dictout['r'],
+                             'm5single_i': dictout['i'],
+                             'm5single_z': dictout['z'],
+                             'm5single_y': dictout['y']})
+        
+
+
+
+
+"""
+        def nvisits_deltam5(m5,m5_median):
+
+            diff = m5-m5_median
+        
+            nv = 10**(0.8*diff)
+
+            #return nv.astype(int)
+            return nv
+"""
