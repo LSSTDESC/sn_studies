c068998df007a3048d19a29d1ca33e04145b41ab
pgris <gris@clermont.in2p3.fr>
2020-01-07 15:59:46 +0100

    add __init__.py and setup.py files

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+
+setup(
+    name='sn_studies',
+    version='0.1',
+    description='Studies for supernovae',
+    url='http://github.com/lsstdesc/sn_studies',
+    author='Philippe Gris',
+    author_email='philippe.gris@clermont.in2p3.fr',
+    license='BSD',
+    packages=['sn_design_dd_survey'],
+    python_requires='>=3.5',
+    zip_safe=False,
+    install_requires=[
+        'sn_tools>=0.1',
+    ],
+)
