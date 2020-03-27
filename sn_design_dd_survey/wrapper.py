9cf808c3d38c74ec92e1c7e8bfd780ed2cbccdac
Philippe Gris <philippe.gris@clermont.in2p3.fr>
2020-03-27 17:40:24 +0100

    add a method to modify the number of visits depending on the filter

--- sn_design_dd_survey/wrapper.py
+++ sn_design_dd_survey/wrapper.py
@@ -747,3 +747,54 @@ class Nvisits_m5:
                              'm5single_z': dictout['z'],
                              'm5single_y': dictout['y']})
     """
+
+
+class Mod_z:
+    """
+    Method to modify values of input file
+
+    """
+
+    def __init__(self, fName):
+
+        # load the file
+        tab = np.load(fName, allow_pickle=True)
+
+        tabdf = pd.DataFrame(np.copy(tab))
+
+        tabmod = tabdf.groupby(['cadence']).apply(
+            lambda x: self.mod(x)).reset_index()
+        ll = []
+        for b in 'grizy':
+            ll.append('Nvisits_{}'.format(b))
+        tabmod['Nvisits'] = tabmod[ll].sum(axis=1)
+
+        self.nvisits = tabmod
+
+    def mod(self, grp):
+        """
+        Method to modify a group
+
+
+        Parameters
+        --------------
+        grp : pandas df group
+
+        Returns
+        -----------
+        modified grp group
+        """
+        for band in 'grizy':
+            what = 'Nvisits_{}'.format(band)
+            idx = grp[what] > 1.e-21
+            idx &= grp[what] < 1.
+            grp.loc[idx, what] = 1.
+
+            if band == 'g' or band == 'r':
+                Index_label = grp[grp[what] < 1.e-10].index.tolist()
+                Index_label_p = grp[grp[what] > 1.e-10].index.tolist()
+                #print('index', Index_label, Index_label_p)
+                grp.loc[Index_label, what] = grp.loc[Index_label_p[-1]][what]
+            #print('io', grp[what])
+
+        return grp
