2768b8e43270234a1e6b270f8c5b5180c9bcfa6c
Philippe Gris <philippe.gris@clermont.in2p3.fr>
2020-04-09 12:01:29 +0200

    developed a GUI using tk to display results

--- sn_design_dd_survey/plot.py
+++ sn_design_dd_survey/plot.py
@@ -1,15 +1,23 @@
-from . import plt
+import matplotlib
+import matplotlib.pyplot as plt
 from scipy.interpolate import interp1d
 from .wrapper import Mod_z
 import tkinter as tk
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+#from tkinter import tkFont
+from tkinter import font as tkFont
+from matplotlib.backends.backend_tkagg import (
+    FigureCanvasTkAgg, NavigationToolbar2Tk)
+# from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import numpy as np
+matplotlib.use('tkagg')
 
 
 class ShowVisits:
-    def __init__(self, name, cadence=1.):
+    def __init__(self, name, cadence=1., zmin=0.1, zmax=0.85):
 
         self.cadence = cadence
+        self.zmin = zmin
+        self.zmax = zmax
         data = Mod_z(
             'Nvisits_cadence_Nvisits_median_m5_filter.npy').nvisits
 
@@ -29,123 +37,159 @@ class ShowVisits:
             self.dictvisits['nvisits_{}'.format(b)] = self.interp_z(
                 sel['z'], sel['Nvisits_{}'.format(b)])
 
+        self.z_nvisits = self.interp_z(sel['Nvisits'], sel['z'])
+
     def interp_z(self, x, y):
         return interp1d(x, y, bounds_error=False, fill_value=0.)
 
-    def plotNvisits(self, ax):
+    def plotNvisits(self):
 
         zstep = 0.005
         zvals = np.arange(self.zmin, self.zmax+zstep, zstep)
 
-        ax.plot(zvals, self.dictvisits['nvisits']
-                (zvals), color='k', label='sum')
+        self.ax.plot(zvals, self.dictvisits['nvisits']
+                     (zvals), color='k', label='sum')
 
         for io, b in enumerate(self.bands):
             key = 'nvisits_{}'.format(b)
-            ax.plot(zvals, self.dictvisits[key](
+            self.ax.plot(zvals, self.dictvisits[key](
                 zvals), color=self.colors[b], label='${}$-band'.format(b))
-        ax.grid()
-        ax.set_xlabel('z')
-        ax.set_ylabel('Nvisits')
-        ax.legend()
-        ax.set_ylim(0,)
+        self.ax.grid()
+        self.ax.set_xlabel('z')
+        self.ax.set_ylabel('Nvisits')
+        self.ax.legend()
+        self.ax.set_ylim(0,)
 
-    def plotzlim(self, ax, z=0.6):
+    def plotzlim(self, z=0.6):
 
         fontsize = 15
 
-        ylims = ax.get_ylim()
+        ylims = self.ax.get_ylim()
         nvisits = int(np.round(self.dictvisits['nvisits'](z)))
         yref = 0.9*ylims[1]
         scale = 0.1*ylims[1]
-        ax.text(0.3, yref, 'Nvisits={}'.format(nvisits), fontsize=fontsize)
-
+        self.ax.text(0.3, yref, 'Nvisits={}'.format(
+            nvisits), fontsize=fontsize)
         for io, b in enumerate(self.bands):
             key = 'nvisits_{}'.format(b)
             nvisits_b = int(np.round(self.dictvisits[key](z)))
-            ax.text(0.3, 0.8*ylims[1]-scale*io,
-                    'Nvisits - ${}$ ={}'.format(b, nvisits_b), fontsize=fontsize, color=self.colors[b])
+            self.ax.text(0.3, 0.8*ylims[1]-scale*io,
+                         'Nvisits - ${}$ ={}'.format(b, nvisits_b), fontsize=fontsize, color=self.colors[b])
 
-        ax.text(0.95*z, 1.5*nvisits, 'z = {}'.format(z), fontsize=fontsize)
-        ax.arrow(z, 1.4*nvisits, 0., -1.4*nvisits,
-                 length_includes_head=True, color='r',
-                 head_length=5, head_width=0.01)
-        ax.set_ylim(0,)
+        self.ax.text(0.95*z, 1.5*nvisits,
+                     'z = {}'.format(z), fontsize=fontsize)
+        self.ax.arrow(z, 1.4*nvisits, 0., -1.4*nvisits,
+                      length_includes_head=True, color='r',
+                      head_length=5, head_width=0.01)
+        self.ax.set_ylim(0,)
+
+    def plotnvisits(self, nvisits):
+
+        # get the redshift from the total number of visits
+
+        zlim = self.z_nvisits(nvisits)
+        self.plotzlim(np.round(zlim, 2))
+
+        self.ax.plot(self.ax.get_xlim(), [nvisits]*2,
+                     color='r', linestyle='--')
+        """
+        self.ax.arrow(0.3, nvisits, -0.3, 0.01*nvisits,
+                      length_includes_head=True, color='r',
+                      head_length=5, head_width=0.01)
+        """
 
     def __call__(self, z=0.6):
 
         root = tk.Tk()
-
-        # fig, ax = plt.subplots(figsize=(11, 6))
-
-        fig = plt.Figure(figsize=(11, 6), dpi=100)
-        ax = fig.add_subplot(111)
+        #boldStyle = ttk.Style()
+        #boldStyle.configure("Bold.TButton", size=10, weight="bold")
+        self.fig = plt.Figure(figsize=(15, 6), dpi=100)
+        self.ax = self.fig.add_subplot(111)
         leg = 'day$^{-1}$'
-        fig.suptitle('cadence: {} {}'.format(int(self.cadence), leg))
-        self.plotNvisits(ax)
+        self.fig.suptitle('cadence: {} {}'.format(int(self.cadence), leg))
+        self.fig.subplots_adjust(right=0.8)
+        self.ax.set_xlim(self.zmin, self.zmax)
+        self.canvas = FigureCanvasTkAgg(self.fig, master=root)
+        self.canvas.draw()
+        self.canvas.get_tk_widget().pack(side=tk.LEFT, fill=tk.BOTH)
+        self.canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=False)
 
-        bar1 = FigureCanvasTkAgg(fig, root)
-        bar1.get_tk_widget().pack(side=tk.LEFT, fill=tk.BOTH)
-        self.updateData(ax, -1)
-        B = tk.Button(root, text="Get visits",
-                      command=self.updateData(ax, z=-1))
+        self.toolbar = NavigationToolbar2Tk(self.canvas, root)
+        self.toolbar.update()
+        # self.ax.cla()
+        self.plotNvisits()
+        # building widgets
+        helv36 = tkFont.Font(family='Helvetica', size=15, weight='bold')
+        # frame
+        button_frame = tk.Frame(master=root, bg="white")
+        button_frame.pack(fill=tk.X, side=tk.BOTTOM, expand=False)
+        button_frame.place(relx=.9, rely=.5, anchor="c")
+        # entries
+        ents = self.make_entries(button_frame, font=helv36)
 
-        # B.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
-        B.pack()
-        entree = tk.Entry(root, textvariable='string', width=30)
-        entree.pack()
-        """
-        tk.Canvas(root, width=250, height=50, bg='ivory').pack(
-            side=tk.LEFT, padx=5, pady=5)
-        
-        for line, item in enumerate(['foo', 'bar', 'stuff']):
-            l = tk.Label(root, text=item, width=10)
-            e = tk.Entry(root, width=10)
-            l.grid(row=line, column=0)
-            e.grid(row=line, column=1)
-        """
+        # buttons
+        heightb = 3
+        widthb = 6
+
+        nvisits_button = tk.Button(
+            button_frame, text="Nvisits", command=(lambda e=ents: self.updateData_z(e)),
+            bg='yellow', height=heightb, width=widthb, fg='blue', font=helv36)
+
+        z_button = tk.Button(button_frame, text="zlim", command=(
+            lambda e=ents: self.updateData_nv(e)), bg='yellow', height=heightb, width=widthb, fg='red', font=helv36)
+
+        quit_button = tk.Button(button_frame, text="Quit",
+                                command=root.quit, bg='yellow',
+                                height=heightb, width=widthb, fg='black', font=helv36)
+
+        button_frame.columnconfigure(0, weight=1)
+        button_frame.columnconfigure(1, weight=1)
+        button_frame.columnconfigure(2, weight=1)
+
+        nvisits_button.grid(row=2, column=0, sticky=tk.W+tk.E)
+        z_button.grid(row=2, column=1, sticky=tk.W+tk.E)
+        quit_button.grid(row=2, column=2, sticky=tk.W+tk.E)
+
         root.mainloop()
 
-    def updateData(self, ax, z):
+    def make_entries(self, frame, font):
 
-        ax.clear()
-        self.plotNvisits(ax)
+        tk.Label(frame, text='zlim', bg='white',
+                 fg='blue', font=font).grid(row=0)
+        tk.Label(frame, text='Nvisits', bg='white',
+                 fg='red', font=font).grid(row=1)
 
+        entries = {}
+        entries['zlim'] = tk.Entry(frame, width=5, font=font)
+        entries['Nvisits'] = tk.Entry(frame, width=5, font=font)
+        # entries['zlim'].pack(ipady=3)
+        entries['zlim'].insert(10, "0.7")
+        entries['Nvisits'] .insert(10, "50")
+        entries['zlim'].grid(row=0, column=1)
+        entries['Nvisits'] .grid(row=1, column=1)
+
+        return entries
+
+    def updateData_z(self, entries):
+
+        self.ax.cla()
+        self.plotNvisits()
+
+        z = float(entries['zlim'].get())
         if z > 0:
-            self.plotzlim(ax, z=z)
+            self.plotzlim(z=z)
+        # update canvas
+        self.ax.set_xlim(self.zmin, self.zmax)
+        self.canvas.draw()
 
-    def call(self, z=0.5):
+    def updateData_nv(self, entries):
 
-        colors = dict(zip(self.bands, ['c', 'g', 'y', 'r', 'm']))
-        fontsize = 15
-        zstep = 0.005
-        zvals = np.arange(self.zmin, self.zmax+zstep, zstep)
+        self.ax.cla()
+        self.plotNvisits()
 
-        fig, ax = plt.subplots(figsize=(11, 6))
-        leg = 'day$^{-1}$'
-        fig.suptitle('cadence: {} {}'.format(int(self.cadence), leg))
-        ax.plot(zvals, self.dictvisits['nvisits']
-                (zvals), color='k', label='sum')
-
-        ylims = ax.get_ylim()
-        nvisits = int(np.round(self.dictvisits['nvisits'](z)))
-        yref = 0.9*ylims[1]
-        scale = 0.1*ylims[1]
-        ax.text(0.3, yref, 'Nvisits={}'.format(nvisits), fontsize=fontsize)
-        for io, b in enumerate(self.bands):
-            key = 'nvisits_{}'.format(b)
-            nvisits_b = int(np.round(self.dictvisits[key](z)))
-            ax.text(0.3, 0.8*ylims[1]-scale*io,
-                    'Nvisits - ${}$ ={}'.format(b, nvisits_b), fontsize=fontsize, color=colors[b])
-            ax.plot(zvals, self.dictvisits[key](
-                zvals), color=colors[b], label='${}$-band'.format(b))
-
-        ax.text(0.95*z, 1.5*nvisits, 'z = {}'.format(z), fontsize=fontsize)
-        ax.arrow(z, 1.4*nvisits, 0., -1.4*nvisits,
-                 length_includes_head=True, color='r',
-                 head_length=5, head_width=0.01)
-        ax.grid()
-        ax.set_xlabel('z')
-        ax.set_ylabel('Nvisits')
-        ax.legend()
-        ax.set_ylim(0,)
+        nv = float(entries['Nvisits'].get())
+        if nv > 0:
+            self.plotnvisits(nvisits=nv)
+        # update canvas
+        self.ax.set_xlim(self.zmin, self.zmax)
+        self.canvas.draw()
