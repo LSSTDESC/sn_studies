f0c729d8be0e67ad6c10385e3179a489fad75527
Philippe Gris <gris@clermont.in2p3.fr>
2020-08-07 17:53:40 +0200

    add init.py file in sn_saturation repo: this a package now!

---
+++ sn_saturation/__init__.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+
+filtercolors = dict(zip('ugrizy', ['b', 'c', 'g', 'y', 'r', 'm']))
+plt.rcParams['xtick.labelsize'] = 15
+plt.rcParams['ytick.labelsize'] = 15
+plt.rcParams['axes.labelsize'] = 15
+plt.rcParams['figure.titlesize'] = 15
+plt.rcParams['legend.fontsize'] = 15
