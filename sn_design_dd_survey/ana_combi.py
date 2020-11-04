e2769e0f1147f9140741afc5d06e27e9ea51ab47
Philippe Gris <philippe.gris@clermont.in2p3.fr>
2020-11-04 14:39:06 +0100

    add a pseudo chiq to select the good SNR combi

--- sn_design_dd_survey/ana_combi.py
+++ sn_design_dd_survey/ana_combi.py
@@ -5,25 +5,25 @@ import multiprocessing
 import os
 import pandas as pd
 
+
 def anafich(tab):
-    idx = tab['Nvisits']<200.
-    idx &= tab['sigmaC']>=0.0390
+    idx = tab['Nvisits'] < 200.
+    idx &= tab['sigmaC'] >= 0.0390
     sel = pd.DataFrame(tab[idx].copy())
     if len(sel) <= 0:
         return None
 
-    #print(sel.columns)
+    # print(sel.columns)
 
-    lliss = ['sigmaC','SNRcalc_tot']
+    lliss = ['sigmaC', 'SNRcalc_tot', 'Nvisits']
     for b in 'grizy':
         lliss += ['Nvisits_{}'.format(b)]
         lliss += ['SNRcalc_{}'.format(b)]
 
-    
     return sel[lliss]
 
 
-def loopAna(fis,j=0, output_q=None):
+def loopAna(fis, j=0, output_q=None):
 
     snr = None
     for fi in fis:
@@ -33,25 +33,26 @@ def loopAna(fis,j=0, output_q=None):
             if snr is None:
                 snr = sel
             else:
-            #print(snr,tab)
+                # print(snr,tab)
                 #snr = np.concatenate((snr,sel))
-                snr = pd.concat((snr,sel))
-                
+                snr = pd.concat((snr, sel))
+
     if output_q is not None:
         return output_q.put({j: snr})
     else:
         return snr
-    
-def multiAna(thedir,nproc=8):
-    
+
+
+def multiAna(thedir, nproc=8):
+
     fis = glob.glob('{}/*.npy'.format(thedir))
     nz = len(fis)
     t = np.linspace(0, nz, nproc+1, dtype='int')
     #print('multi', nz, t)
     result_queue = multiprocessing.Queue()
-    
+
     procs = [multiprocessing.Process(name='Subprocess-'+str(j), target=loopAna,
-                                         args=(fis[t[j]:t[j+1]], j, result_queue))
+                                     args=(fis[t[j]:t[j+1]], j, result_queue))
              for j in range(nproc)]
 
     for p in procs:
@@ -78,7 +79,8 @@ def multiAna(thedir,nproc=8):
                 #restot = np.concatenate((restot, vals))
                 restot = pd.concat((restot, vals))
     return restot
-    
+
+
 z = 0.8
 tag = 'z_{}'.format(z)
 thedir = 'SNR_combi/{}'.format(tag)
@@ -87,45 +89,69 @@ sumname = 'summary_{}.npy'.format(tag)
 
 if not os.path.isfile(sumname):
     snr = multiAna(thedir)
-    print('type',type(snr))
-    np.save(sumname,snr.to_records(index=False))
+    print('type', type(snr))
+    np.save(sumname, snr.to_records(index=False))
 
-snr = np.load(sumname,allow_pickle=True)
-    
-print(snr.dtype)
+snr = pd.DataFrame(np.load(sumname, allow_pickle=True))
+
+print(snr.columns)
 """
 plt.plot(snr['Nvisits'],snr['sigmaC'],'r*')
 plt.show()
 """
 
-fluxFrac = np.load('fracSignalBand.npy',allow_pickle=True)
+fluxFrac = np.load('fracSignalBand.npy', allow_pickle=True)
 
 print(fluxFrac.dtype)
-idb = np.abs(fluxFrac['z']-z)<1.e-8
+idb = np.abs(fluxFrac['z']-z) < 1.e-8
 fluxFrac = fluxFrac[idb]
 
 
 print(fluxFrac)
 
-r = []
-for b in 'grizy':
-    io = fluxFrac['band']==b
+bbands = 'grizy'
+for b in bbands:
+    io = fluxFrac['band'] == b
     flux = fluxFrac[io]
-    diff = snr['SNRcalc_{}'.format(b)]/snr['SNRcalc_tot']
-    plt.plot(snr['Nvisits_{}'.format(b)],diff)
+    print('flux', flux.dtype)
+    snr['chisq_{}'.format(b)] = snr['SNRcalc_{}'.format(b)] / \
+        snr['SNRcalc_tot']
+    snr['chisq_{}'.format(b)] -= flux['fracfluxband']
+    #plt.plot(snr['Nvisits_{}'.format(b)], snr['chiq_{}'.format(b)])
 
+# plt.show()
+
+snr['chisq'] = 0.
+for b in bbands:
+    snr['chisq'] += snr['chisq_{}'.format(b)] * snr['chisq_{}'.format(b)]
+
+snr['chisq'] = np.sqrt(snr['chisq'])
+
+snr = snr.sort_values(by=['chisq'])
+
+#ij = snr['chisq'] <= 0.5
+ij = snr['Nvisits_y'] <= 25.
+ij &= snr['Nvisits_i'] >= snr['Nvisits_r']
+ij &= snr['Nvisits_z'] >= snr['Nvisits_i']
+snr = snr[ij]
+
+idxa = np.argmin(snr['chisq'])
+print(snr.iloc[idxa])
+
+# print(snr[:20])
+plt.plot(snr['chisq'], snr['Nvisits'], 'ko')
 plt.show()
+idxa = np.argmin(sel_snr['Nvisits'])
+print(sel_snr.iloc[idxa])
+print(snr)
 
 
-
-idx = snr['Nvisits_y']>=5
-idx &= snr['Nvisits_y']<=20
-idx &= snr['Nvisits_i']>=10
-idx &= snr['Nvisits_i']>=snr['Nvisits_r']
-idx &= snr['Nvisits_z']>=snr['Nvisits_i']
+idx = snr['Nvisits_y'] >= 5
+idx &= snr['Nvisits_y'] <= 20
+idx &= snr['Nvisits_i'] >= 10
+idx &= snr['Nvisits_i'] >= snr['Nvisits_r']
+idx &= snr['Nvisits_z'] >= snr['Nvisits_i']
 sel_snr = snr[idx]
 
-idxa = np.argmin(sel_snr['Nvisits'])
-print(sel_snr[idxa])
-#plt.plot(sel_snr['Nvisits'],sel_snr['sigmaC'],'ko',mfc='None')
 
+# plt.plot(sel_snr['Nvisits'],sel_snr['sigmaC'],'ko',mfc='None')
