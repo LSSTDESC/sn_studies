b5f18b70399b8add212ed8b324d7874285ea515f
pgris <gris@clermont.in2p3.fr>
2021-01-18 15:54:52 +0100

    add estimation of m5 single visit

--- sn_DD_nsn/plot_cadence.py
+++ sn_DD_nsn/plot_cadence.py
@@ -15,6 +15,10 @@ def calc(grp, pixArea):
     frac_missing_nights = 1.-(nights_med/(season_length_med/cadence_med))
     area = np.median(grp.groupby(['season']).size())*pixArea
 
+    m5_single = {}
+    for b in 'grizy':
+        m5_single[b] = grp['m5_single_{}'.format(b)].median()
+
     return pd.DataFrame({'nights': [nights_med],
                          'season_length': [int(season_length_med)],
                          'cadence': [cadence_med],
@@ -22,8 +26,12 @@ def calc(grp, pixArea):
                          'medgap': [grp['medgap'].median()],
                          'missing_nights': [missing_nights],
                          'frac_missing_nights': [frac_missing_nights],
-                         'area': [area]})
-
+                         'area': [area],
+                         'm5_single_g':[m5_single['g']],
+                         'm5_single_r':[m5_single['r']],
+                         'm5_single_i':[m5_single['i']],
+                         'm5_single_z':[m5_single['z']],
+                         'm5_single_y':[m5_single['y']]})
 
 def plot(ax, df, var):
 
@@ -40,7 +48,11 @@ def load(dbName, nside, group=['fieldName']):
         dbName, nside), allow_pickle=True))
 
     pixArea = hp.nside2pixarea(nside, degrees=True)
-
+    
+    for b in 'grizy':
+        df['m5_single_{}'.format(b)] =  df['m5_{}'.format(b)]-1.25*np.log10(df['N_{}'.format(b)])
+    
+    print(df.columns)
     dfb = df.groupby(group).apply(lambda x: calc(x, pixArea)).reset_index()
 
     return dfb
@@ -65,15 +77,21 @@ dbNames = ['baseline_v1.5_10yrs',
            # 'dm_heavy_nexp2_v1.6_10yrs',
            'dm_heavy_v1.6_10yrs',
            # 'ddf_heavy_nexp2_v1.6_10yrs',
-           'ddf_heavy_v1.6_10yrs']
+           'ddf_heavy_v1.6_10yrs',
+           'cadence_drive_gl200_gcbv1.6.1_10yrs']
 
 nside = 128
 dfb = load_all(dbNames, nside)
 
 vars_toplot = ['nights', 'season_length', 'cadence',
                'maxgap', 'medgap', 'missing_nights', 'frac_missing_nights', 'area']
+for b in 'grizy':
+    vars_toplot += ['m5_single_{}'.format(b)]
+
 legend = ['#nights/season/pixel', 'season length [day]', 'cadence [day-1]',
           'max gap [day]', 'median gap [day]', 'missing nights', 'frac missing_nights', 'area [deg2]']
+for b in 'grizy':
+    legend += ['m5 single visit - {} band'.format(b)]
 
 toplot = dict(zip(vars_toplot, legend))
 
