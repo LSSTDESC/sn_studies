42ed1cc638e35a5fe037fc3a80c1ea50e22b0e5f
Philippe Gris <philippe.gris@clermont.in2p3.fr>
2021-05-31 12:35:17 +0200

    code update

--- sn_DD_nsn/cadence_obspix_slidwind.py
+++ sn_DD_nsn/cadence_obspix_slidwind.py
@@ -26,7 +26,7 @@ class Coadd:
 
     def __init__(self, dirFiles, dbName, fieldName, dirOut='.'):
 
-        outName = '{}/{}_coadd.npy'.format(dirOut, dbName)
+        outName = '{}/{}_{}_coadd.npy'.format(dirOut, dbName, fieldName)
 
         if not os.path.isfile(outName):
             # load data
@@ -137,36 +137,56 @@ class ObsSlidingWindow:
         zvals[0] = 0.01
         for z in zvals:
             # for each redshift: get T0s
+
             T0_min = season_min-self.min_rf_qual*(1.+z)
             T0_max = season_max-self.max_rf_qual*(1.+z)
             for T0 in np.arange(T0_min, T0_max, 3.):
+                r_infos = [healpixID, season, z, T0, cadence_season]
                 sel = self.cutoff(selobs, T0, z)
-                nepochs_bef, nepochs_aft, cadence = self.infos(sel, T0)
-                print(z, T0, len(sel), nepochs_bef, nepochs_aft, cadence)
-                r.append((healpixID, season, z, T0,
-                          nepochs_bef, nepochs_aft, cadence, cadence_season))
+                r_infos += self.infos(sel, T0, z)
+
+                r.append(r_infos)
 
         rr = None
         if len(r) > 0:
             rr = np.rec.fromrecords(r, names=[
-                                    'healpixID', 'season', 'z', 'T0', 'nepochs_bef', 'nepochs_aft', 'cadence', 'cadence_season'])
+                                    'healpixID', 'season', 'z', 'T0', 'cadence_season',
+                                    'nepochs_bef', 'nepochs_aft',
+                                    'cadence', 'cadence_std',
+                                    'nphase_min', 'nphase_max', 'max_gap'])
 
         return rr
 
-    def infos(self, obs, T0):
+    def infos(self, obs, T0, z, phase_min=-10, phase_max=20):
 
         df = pd.DataFrame(np.copy(obs))
         dd = df.groupby(['night'])[self.mjdCol].median().reset_index()
 
-        dd = dd.sort_values(by=[self.mjdCol])
-        idx = dd[self.mjdCol]-T0 <= 0
-        nepochs_bef = len(dd[idx])
-        nepochs_aft = len(dd)-nepochs_bef
         cadence = -1.0
+        nphase_min = -1
+        nphase_max = -1
+        nepochs_bef = -1
+        nepochs_aft = -1
+        max_gap = -1.
+        cadence_std = -1.
         if len(dd) >= 2:
-            cadence = np.mean(np.diff(dd[self.mjdCol]))
+            dd['phase'] = (T0-dd[self.mjdCol])/(1.+z)
+            dd = dd.sort_values(by=[self.mjdCol])
+            idx = dd[self.mjdCol]-T0 <= 0
+            nepochs_bef = len(dd[idx])
+            nepochs_aft = len(dd)-nepochs_bef
+            idx = dd['phase'] <= phase_min
+            nphase_min = len(dd[idx])
+            idx = dd['phase'] >= phase_max
+            nphase_max = len(dd[idx])
+            diff_mjd = np.diff(dd[self.mjdCol])
+            cadence = np.mean(diff_mjd)
+            cadence_std = np.std(diff_mjd)
+            max_gap = np.max(diff_mjd)
 
-        return nepochs_bef, nepochs_aft, cadence
+        r = [nepochs_bef, nepochs_aft, cadence, cadence_std,
+             nphase_min, nphase_max, max_gap]
+        return r
 
     def cutoff(self, obs, T0, z):
         """ select observations depending on phases
@@ -198,6 +218,106 @@ class ObsSlidingWindow:
         return obs[idx]
 
 
+class Analysis:
+    """
+    """
+
+    def __init__(self, data_in, dbName, fieldName,
+                 nepochs_bef=4, nepochs_aft=10, nphase_min=1, nphase_max=1):
+
+        self.nepochs_bef = nepochs_bef
+        self.nepochs_aft = nepochs_aft
+        self.nphase_min = nphase_min
+        self.nphase_max = nphase_max
+
+        # get the data
+        self.fName = '{}_{}_slidingWindow.npy'.format(dbName, fieldName)
+        if not os.path.isfile(self.fName):
+            self.slidingWindow(data_in)
+
+        data = np.load(self.fName, allow_pickle=True)
+
+        res = pd.DataFrame(data).groupby(['healpixID', 'season']).apply(
+            lambda x: self.effiObs(x)).reset_index()
+        print(res['healpixID'].unique())
+
+        for healpixID in res['healpixID'].unique():
+            ida = res['healpixID'] == healpixID
+            sel = res[ida]
+            print(sel)
+            fig, ax = plt.subplots()
+            for season in sel['season'].unique():
+                io = sel['season'] == season
+                self.plot_indiv(ax, sel[io])
+
+        plt.show()
+
+    def slidingWindow(self, data):
+        restot = None
+        cad = ObsSlidingWindow()
+        for healpixID in np.unique(data['healpixID']):
+            idx = data['healpixID'] == healpixID
+            rr = cad(data[idx], healpixID)
+            if restot is None:
+                restot = rr
+            else:
+                restot = np.concatenate((restot, rr))
+
+        np.save(self.fName, restot)
+
+    def effiObs(self, df):
+        """
+        Method to estimate observing efficiency vs z
+
+        Returns
+        -----------
+        pandasdf with z, effi, var(effi) as columns
+        """
+        #df = pd.DataFrame(self.data.copy)
+
+        # apply selection
+        idx = df['nepochs_bef'] >= self.nepochs_bef
+        idx &= df['nepochs_aft'] >= self.nepochs_aft
+        idx &= df['nphase_min'] >= self.nphase_min
+        idx &= df['nphase_max'] >= self.nphase_max
+
+        df_sel = df[idx]
+
+        # make groups (with z)
+        group = df.groupby('z')
+        group_sel = df_sel.groupby('z')
+
+        # Take the ratio to get efficiencies
+        rb = (group_sel.size()/group.size())
+        #err = np.sqrt(rb*(1.-rb)/group.size())
+        var = rb*(1.-rb)*group.size()
+
+        rb = rb.array
+        #err = err.array
+        var = var.array
+
+        rb[np.isnan(rb)] = 0.
+        #err[np.isnan(err)] = 0.
+        var[np.isnan(var)] = 0.
+
+        return pd.DataFrame({group.keys: list(group.groups.keys()),
+                             'effi': rb,
+                             # 'effi_err': err,
+                             'effi_var': var})
+
+    def plot_indiv(self, ax, effis):
+        """
+        Method to plot efficiency vs redshift
+
+        Parameters
+        ---------------
+        effis: pandas df
+           data to plot
+        """
+        print('hello', effis)
+        ax.errorbar(effis['z'], effis['effi'], yerr=np.sqrt(effis['effi_var']))
+
+
 dirFiles = '../../ObsPixelized_128'
 dbName = 'descddf_v1.5_10yrs'
 
@@ -208,16 +328,7 @@ data = Coadd(dirFiles, dbName, fieldName).data
 data = season(data)
 print(data.dtype)
 
-restot = None
-cad = ObsSlidingWindow()
-for healpixID in np.unique(data['healpixID']):
-    idx = data['healpixID'] == healpixID
-    rr = cad(data[idx], healpixID)
-    if restot is None:
-        restot = rr
-    else:
-        restot = np.concatenate((restot, rr))
-    break
+restot = Analysis(data, dbName, fieldName).data
 
 print(restot)
 
@@ -228,8 +339,9 @@ for season in np.unique(restot['season']):
     ssel = restot[idx]
     print('season', season, np.unique(
         ssel['cadence_season']), np.median(ssel['cadence']))
-    ax.plot(ssel['z'], ssel['nepochs_bef'], 'ko')
-    ax.plot(ssel['z'], ssel['nepochs_aft'], 'ro')
+    #ax.plot(ssel['z'], ssel['nepochs_bef'], 'ko')
+    #ax.plot(ssel['z'], ssel['nepochs_aft'], 'ro')
+    ax.plot(ssel['z'], ssel['max_gap'], 'ko')
 
     # break
 
