c77973c3a7c80ef6594d58f0f091c4d22c4a206b
Philippe Gris <philippe.gris@clermont.in2p3.fr>
2021-06-04 16:10:36 +0200

    add binned plots

--- sn_DD_nsn/compare_metric_fake.py
+++ sn_DD_nsn/compare_metric_fake.py
@@ -3,6 +3,9 @@ from sn_tools.sn_io import loopStack
 import glob
 import matplotlib.pyplot as plt
 from optparse import OptionParser
+import scipy.stats
+import numpy as np
+
 
 def loadMetric(metricDir, dbName):
 
@@ -25,10 +28,27 @@ def loadFakes(fakeDir, dbName):
 
     return df
 
+
+def plotBinned(ax, metricTot, xp='cadence', yp='nsn_med_faint', label='', bins=10, therange=(1., 10.)):
+
+    x = metricTot[xp]
+    y = metricTot[yp]
+
+    means_result = scipy.stats.binned_statistic(
+        x, [y, y**2], bins=10, range=therange, statistic='mean')
+    means, means2 = means_result.statistic
+    standard_deviations = np.sqrt(means2 - means**2)
+    bin_edges = means_result.bin_edges
+    bin_centers = (bin_edges[:-1] + bin_edges[1:])/2.
+
+    ax.errorbar(x=bin_centers, y=means, yerr=standard_deviations,
+                marker='.', label=label)
+
+
 parser = OptionParser()
 
 parser.add_option(
-    '--metricDir', help='metric directory [%default]', default='../MetricOutput_DD_new_128',type=str)
+    '--metricDir', help='metric directory [%default]', default='../MetricOutput_DD_new_128', type=str)
 parser.add_option(
     '--dbName', help='OS to process [%default]', default='daily_ddf_v1.5_10yrs', type=str)
 parser.add_option(
@@ -41,7 +61,27 @@ metricDir = opts.metricDir
 dbName = opts.dbName
 fakeDir = opts.fakeDir
 
-df_metric = loadMetric(metricDir, dbName)
+df_ = loadMetric(metricDir, dbName)
+idx = df_['nsn_med_faint'] >= 0
+df_metric = df_[idx]
+df_metric['nsn_med_faint_norm'] = 180. * \
+    df_metric['nsn_med_faint'] / df_metric['season_length']
+print(df_metric.columns)
+healpixID = 144428
+
+idx = df_metric['healpixID'] == healpixID
+vars = ['healpixID', 'season', 'cadence',
+        'zlim_faint', 'nsn_med_faint', 'nsn_med_faint_norm', 'season_length', 'gap_max', 'gap_med']
+print(df_metric[idx][vars])
+
+plt.plot(df_metric['gap_max'], df_metric['cadence'], 'ko')
+
+fig, ax = plt.subplots()
+
+plotBinned(ax, df_metric, xp='gap_max', yp='cadence',
+           label='', bins=10, therange=(1., 50.))
+plt.show()
+"""
 df_fakes = loadFakes(fakeDir, dbName)
 
 print(df_metric)
@@ -62,5 +102,5 @@ fig, ax = plt.subplots()
 
 ax.plot(df['zlimdiff'], df['nsnrat'], 'ko')
 
-
 plt.show()
+"""
