ad2da801554633a554c5967e1b7888a7772c8ed3
Philippe Gris <gris@clermont.in2p3.fr>
2021-10-18 17:45:38 +0200

    update plot size params

--- sn_fom/__init__.py
+++ sn_fom/__init__.py
@@ -2,11 +2,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 filtercolors = dict(zip('ugrizy', ['b', 'c', 'g', 'y', 'r', 'm']))
-plt.rcParams['xtick.labelsize'] = 15
-plt.rcParams['ytick.labelsize'] = 15
-plt.rcParams['axes.labelsize'] = 15
-plt.rcParams['figure.titlesize'] = 15
-plt.rcParams['legend.fontsize'] = 15
+plt.rcParams['xtick.labelsize'] = 20
+plt.rcParams['ytick.labelsize'] = 20
+plt.rcParams['axes.labelsize'] = 25
+plt.rcParams['figure.titlesize'] = 25
+plt.rcParams['legend.fontsize'] = 25
 #plt.rcParams['font.weight'] = 'bold'
 #plt.rcParams['axes.labelweight'] = 'bold'
 plt.rcParams['font.family'] = 'Arial'
