9526a81be8d16717e59f6d0ddbe4c48a65ec803a
Philippe Gris <philippe.gris@clermont.in2p3.fr>
2022-01-07 15:15:12 +0100

    number of configs extended

--- sn_fom/gime_nsn_sigma_mu.py
+++ sn_fom/gime_nsn_sigma_mu.py
@@ -22,7 +22,7 @@ parser.add_option("--outName_nsn", type="str",
 parser.add_option("--Ny", type=int, default=40,
                   help="y-band visits max at 0.9 [%default]")
 parser.add_option("--dbNames_all", type=str,
-                  default='DD_0.65,DD_0.70,DD_0.75,DD_0.80,DD_0.85,DD_0.90',
+                  default='DD_0.50,DD_0.55,DD_0.60,DD_0.65,DD_0.70,DD_0.75,DD_0.80,DD_0.85,DD_0.90',
                   help="dbNames to consider to estimate reference files [%default]")
 
 opts, args = parser.parse_args()
@@ -33,6 +33,7 @@ fileDir = opts.fileDir
 dbNames = opts.dbName.split('/')
 dbNames_all = opts.dbNames_all .split(',')
 outName = '{}.hdf5'.format(opts.outName_sigmamu)
+print('sigma mu', outName)
 sigma_mu_from_simu = Sigma_mu_obs(fileDir,
                                   dbNames=dbNames_all,
                                   snTypes=['allSN']*len(dbNames_all),
@@ -48,6 +49,7 @@ config = getconfig(['DD_0.90'],
 
 #outName = 'nsn_bias_Ny_{}.hdf5'.format(Ny)
 outName = '{}.hdf5'.format(opts.outName_nsn)
+print('nsn bias', outName)
 nsn_bias = NSN_bias(fileDir, config, fields=['COSMOS', 'XMM-LSS', 'CDFS', 'ADFS', 'ELAIS'],
                     dbNames=dbNames_all,
                     plot=False, outName=outName).data
