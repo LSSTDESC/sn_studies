5a6f71351ae1ae516e4005aa7fba3e129c275eb4
Philippe Gris <gris@clermont.in2p3.fr>
2022-01-10 17:34:12 +0100

    changed default process

--- sn_fom/scripts_util/loop_cosmoplot.sh
+++ sn_fom/scripts_util/loop_cosmoplot.sh
@@ -11,9 +11,9 @@ python sn_studies/sn_fom/cosmo_plot.py --fileDir cosmofit/Fit_bias_Ny_40_Om_w0 -
 
 #nominal
 
-#python sn_studies/sn_fom/cosmo_plot.py --fileDir cosmofit/Fit_bias_Ny_40_Om_w0 --config config_cosmoSN_universal_yearly.csv --runtype universal --outName cosmoSN_universal_yearly_Ny_40
-#python sn_studies/sn_fom/cosmo_plot.py --fileDir cosmofit/Fit_bias_Ny_40_Om_w0 --config config_cosmoSN_deep_rolling_2_2_mini_yearly.csv --runtype deep_rolling --outName cosmoSN_deep_rolling_2_2_mini_yearly_Ny_40
-#python sn_studies/sn_fom/cosmo_plot.py --fileDir cosmofit/Fit_bias_Ny_40_Om_w0 --config config_cosmoSN_deep_rolling_0.80_0.80_yearly.csv --runtype deep_rolling --outName cosmoSN_deep_rolling_0.80_0.80_yearly_Ny_40
+python sn_studies/sn_fom/cosmo_plot.py --fileDir cosmofit/Fit_bias_Ny_40_Om_w0 --config config_cosmoSN_universal_yearly.csv --runtype universal --outName cosmoSN_universal_yearly_Ny_40
+python sn_studies/sn_fom/cosmo_plot.py --fileDir cosmofit/Fit_bias_Ny_40_Om_w0 --config config_cosmoSN_deep_rolling_2_2_mini_yearly.csv --runtype deep_rolling --outName cosmoSN_deep_rolling_2_2_mini_yearly_Ny_40
+python sn_studies/sn_fom/cosmo_plot.py --fileDir cosmofit/Fit_bias_Ny_40_Om_w0 --config config_cosmoSN_deep_rolling_0.80_0.80_yearly.csv --runtype deep_rolling --outName cosmoSN_deep_rolling_0.80_0.80_yearly_Ny_40
 
 # bias 2 sigmas
 #python sn_studies/sn_fom/cosmo_plot.py --fileDir fake/Fit_bias_bias_2_sigma_Ny_40_Om_w0 --config config_cosmoSN_universal_yearly.csv --runtype universal --outName cosmoSN_universal_yearly_Ny_40_bias_2_sigma
